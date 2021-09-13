1c5eb0ef3b4995c584fe51e20b8891208e60a210
bog26 <l_bogdan2001@yahoo.com>
2021-09-13 23:36:03 +0200

    user data input updates

---
+++ PSO/Migrations/202109132133440_DataInputUpdate.cs
@@ -0,0 +1,22 @@
+﻿namespace PSO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataInputUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Clients", "ClientPersonalData_Id", c => c.Int());
+            CreateIndex("dbo.Clients", "ClientPersonalData_Id");
+            AddForeignKey("dbo.Clients", "ClientPersonalData_Id", "dbo.UserPersonalDatas", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Clients", "ClientPersonalData_Id", "dbo.UserPersonalDatas");
+            DropIndex("dbo.Clients", new[] { "ClientPersonalData_Id" });
+            DropColumn("dbo.Clients", "ClientPersonalData_Id");
+        }
+    }
+}
