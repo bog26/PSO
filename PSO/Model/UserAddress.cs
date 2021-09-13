d842575e583c411e0d872672ae78db60a3e4982c
bog26 <l_bogdan2001@yahoo.com>
2021-09-13 14:55:42 +0200

    new classes

---
+++ PSO/Model/UserAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSO.Model
+{
+    class UserAddress
+    {
+        private string street;
+        private int streetNr;
+        private string city;
+        private string region;
+        private string country;
+        private int postalCode;
+
+    }
+}
---
+++ PSO/Model/UserPersonalData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSO.Model
+{
+    public class UserPersonalData
+    {
+        private string FirstName;
+        private string LastName;
+        private DateTime BirthDate;
+        private string email;
+        private UserAddress address;
+
+    }
+}
