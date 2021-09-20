914b620bd8443169f08dd013c006a29c731f06c4
bog26 <l_bogdan2001@yahoo.com>
2021-09-21 00:00:32 +0200

    Status class added

---
+++ PSO/Model/Status.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSO.Model
+{
+    class Status
+    {
+        private string crtStatus;
+        private List<string> transactionStatuses = new List<string>()
+        { "Requested", "Accepted", "Completed"};
+        private List<string> messageStatuses = new List<string>()
+        { "sent", "received", "read"};
+        public Status(Transaction tr)
+        {
+            this.crtStatus = this.transactionStatuses[0];
+        }
+        public string CrtStatus
+        { 
+            get{ return this.crtStatus; }
+            set {this.crtStatus = value; } 
+        }
+    public void ChangeTransactionStatus(int i)
+        {
+            if (i == transactionStatuses.Count - 1)
+            {
+                this.crtStatus = transactionStatuses[transactionStatuses.Count - 1];
+            }
+            else 
+            {
+                this.crtStatus = transactionStatuses[i++];
+            }
+            
+            
+
+        }
+    }
+}
