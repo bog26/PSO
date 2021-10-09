c08235f7f3100a1c7e2827d882d5b76e3d75ec2f
bog26 <l_bogdan2001@yahoo.com>
2021-10-10 00:46:14 +0200

    encryption class done

--- PSO/Model/Encryption.cs
+++ PSO/Model/Encryption.cs
@@ -3,21 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace PSO.Model
 {
     public class Encryption
     {
-        public static string StringEncrypt(string clearText)
+        //https://stackoverflow.com/questions/10168240/encrypting-decrypting-a-string-in-c-sharp
+        public static string StringEncrypt(string clearText, string EncryptionKey)
         {
-            string encryptedText = "";
-
+            string encryptedText;
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    encryptedText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
             return encryptedText;
         }
-        public static string StringDecrypt(string clearText)
+        public static string StringDecrypt(string encryptedText, string EncryptionKey)
         {
-            string decryptedText = "";
-
+            string decryptedText;
+            encryptedText = encryptedText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(encryptedText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    decryptedText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
             return decryptedText;
         }
     }
