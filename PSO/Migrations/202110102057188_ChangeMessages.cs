b80fb12a061eceff8a7b13c762869125c7b8f221
bog26 <l_bogdan2001@yahoo.com>
2021-10-10 23:23:34 +0200

    new messaging and new ICommunication interface

---
+++ PSO/Migrations/202110102057188_ChangeMessages.cs
@@ -0,0 +1,24 @@
+﻿namespace PSO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeMessages : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Messages", "Sender", c => c.String());
+            AddColumn("dbo.Messages", "Receiver", c => c.String());
+            DropColumn("dbo.Messages", "SenderID");
+            DropColumn("dbo.Messages", "ReceiverID");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Messages", "ReceiverID", c => c.Int(nullable: false));
+            AddColumn("dbo.Messages", "SenderID", c => c.Int(nullable: false));
+            DropColumn("dbo.Messages", "Receiver");
+            DropColumn("dbo.Messages", "Sender");
+        }
+    }
+}
--- PSO/Model/Message.cs
+++ PSO/Model/Message.cs
@@ -9,14 +9,23 @@ namespace PSO.Model
     public class Message
     {
         public int Id { get; set; }
-        public int SenderID { get; set; }
-        public int ReceiverID { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+
         public string MessageTitle { get; set; }
         public string MessageBody { get; set; }
-        public void MessageSend()
+        /*
+        public void MessageSend(string title, string body)
         {
 
         }
+        public string GetMessage()
+        {
+            string newMessage = "";
+
+            return newMessage;
+        }
+        */
 
     }
 }
